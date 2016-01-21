57289dc0b8c8d0f8c364c51d49dd956981f22ca4
Jacob Clark <jacob.clark@bbc.co.uk>
2016-01-21 12:11:29 +0000

    Render list of all records

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,8 +1,36 @@
 import {Component} from 'angular2/core';
+import {Http, Headers} from 'angular2/http';
 
 @Component({
     selector: 'playlistr',
-    template: '<p>Hello World, finally.</p>'
+    template: `
+        <h1>Playlistr</h1>
+        <ul>
+            <li *ngFor="#release of releases">
+                {{release.basic_information.title}}
+            </li>
+        </ul>
+    `,
 })
 export class AppComponent {
+    result: Object;
+    releases: Array;
+
+    constructor(http: Http) {
+        this.result = {};
+        http
+            .get(
+                'https://api.discogs.com/users/imjacobclark/collection/folders/0/releases'
+            )
+            .subscribe(
+                data => this.result = JSON.parse(data.text()),
+                err => this.logError(err.text()),
+                () => this.gotRecords(this.result)
+            );
+    }
+
+    gotRecords(records){
+        this.releases = records.releases;
+    }
+
 }
--- src/boot.ts
+++ src/boot.ts
@@ -1,7 +1,8 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from './app.component'
 import {enableProdMode} from 'angular2/core';
+import {HTTP_PROVIDERS} from 'angular2/http';
 
 enableProdMode();
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [HTTP_PROVIDERS]);
