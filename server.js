37394b36bcbf862f15f5782cdf1aa2264da15648
Jacob Clark <jacob.jh.clark@googlemail.com>
2016-01-31 19:31:55 +0000

    Whitelist certain API urls

--- server.js
+++ server.js
@@ -9,19 +9,26 @@ app.use(express.static('./'));
 app.listen(3000);
 
 app.get('/proxy', function(req, res){
-    console.log(req.query.url + '?key=' + process.env.DISCOGS_KEY + '&secret=' + process.env.DISCOGS_SECRET);
-    let options = {
-        url: req.query.url + '?key=' + process.env.DISCOGS_KEY + '&secret=' + process.env.DISCOGS_SECRET,
-        headers: {
-            'User-Agent': 'Playlistr/1.0'
-        }
-    };
-    
-    request(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            res.end(body);
-        }
-    })
+    if(
+        req.query.url != undefined && 
+        req.query.url.indexOf('https://api.discogs.com') > -1 ||
+        req.query.url.indexOf('/collection/folders/0/releases' > -1
+    )){
+        let options = {
+            url: req.query.url + '?key=' + process.env.DISCOGS_KEY + '&secret=' + process.env.DISCOGS_SECRET,
+            headers: {
+                'User-Agent': 'Playlistr/1.0'
+            }
+        };
+        
+        request(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                res.end(body);
+            }
+        });
+    }else{
+        res.end();
+    }
 });
 
-console.log("Playlistr listening on port 3000")
+console.log("Playlistr listening on port 3000");
